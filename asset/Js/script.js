07d4ac44eba12b1f317bbf889115d35ab48ce00f
Cboukhal <boukhalfa.camil@hotmail.fr>
2025-05-28 10:07:43 +0200

    Update script.js

--- asset/Js/script.js
+++ asset/Js/script.js
@@ -7,4 +7,5 @@ $('ul li:first-child').css('font-size','1.5em').css('text-align','center').css('
 
 //Methode html
 $('#un').html("Je suis le H2 modifier depuis le Jquerry");//on peut enchainer en CSS mais pas en HTML.
+//Methode text
 $('ul li:last-child').text("Je suis le li du Jquery");
