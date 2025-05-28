2ab03dd98969fe81adbeb0953a04b72ddb00c56b
Cboukhal <boukhalfa.camil@hotmail.fr>
2025-05-28 10:03:02 +0200

    Jquerry

--- asset/Js/script.js
+++ asset/Js/script.js
@@ -1,3 +1,9 @@
 //$('selecteur').methode() //possibilité d'enchainer les méthodes sauf pour le HTML
 $('*').css('margin','0').css('padding','0').css('background-color','gold');
 $('H1').css('font-size','4em').css('text-align','center').css('color','crimson').css('padding','20px');
+$('#deux').css('font-size','3em').css('text-align','center').css('color','green').css('padding','10px').css('background-color','whitesmoke');
+$('.trois').css('font-size','1.5em').css('text-align','center').css('color','grey').css('padding','10px');
+$('ul li:first-child').css('font-size','1.5em').css('text-align','center').css('color','red').css('padding','5px').css('background-color','purple');
+
+//Methode html
+$('#un').html("Je suis le H2 modifier depuis le Jquerry");
