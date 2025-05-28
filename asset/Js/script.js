5f43a89ffe5bfd2a1598119cb194227092c9131a
Cboukhal <boukhalfa.camil@hotmail.fr>
2025-05-28 09:48:23 +0200

    Update script.js

--- asset/Js/script.js
+++ asset/Js/script.js
@@ -1,2 +1,3 @@
 //$('selecteur').methode() //possibilité d'enchainer les méthodes sauf pour le HTML
 $('*').css('margin','0').css('padding','0').css('background-color','gold');
+$('H1').css('font-size','4em').css('text-align','center').css('color','crimson').css('padding','20px');
